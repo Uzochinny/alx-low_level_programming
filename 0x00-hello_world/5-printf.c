acaa8a432a6d97be7a51a6713d52597da2dd72a8
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-02-16 14:00:09 -0800

    Hello_printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - my program entry point
+ *
+ * Return: returns the value of the argument
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
