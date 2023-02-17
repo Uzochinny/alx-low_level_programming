d835b292ed8b1bb9476630360c42c79881a54189
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-02-16 22:23:02 -0800

    unix2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 /**
  * main - A c program that prints out a line to the standard
- * Return: one
+ * Return: one (success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
+}
