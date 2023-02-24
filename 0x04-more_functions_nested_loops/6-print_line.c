1d8d02920d67ffc61cb121a2dfa9075f9b97a73d
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-02-24 04:27:01 -0800

    function added

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,7 @@ void print_line(int n)
 {
 	if (n <= 0)
 {
-	_putchar(‘\n’);
+	_putchar('\n');
 }
 	else
 {
@@ -16,9 +16,9 @@ void print_line(int n)
 
 	for (i = 1; i <= n; i++)
 	{
-		_putchar(‘_’);
+		_putchar('_');
 	}
-	_putchar(‘\n’);
+	_putchar('\n');
 }
 }
 
