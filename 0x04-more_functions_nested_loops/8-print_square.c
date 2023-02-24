2f4575fd979252ed90c96428c8f5d9abd0c2d994
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-02-24 05:23:50 -0800

    prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-		_putchar(‘\n’);
+		_putchar('\n');
 	}
 	else
 	{
@@ -20,9 +20,9 @@ void print_square(int size)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar(‘#’);
+				_putchar('#');
 			}
-			-putchar(‘\n’);
+			_putchar('\n');
 		}
 	}
 }
