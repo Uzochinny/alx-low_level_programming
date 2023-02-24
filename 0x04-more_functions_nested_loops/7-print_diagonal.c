062b21180223f31b4febb812770e99ab08c1f116
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-02-24 05:15:35 -0800

    draws a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_putchar(‘\n’);
+		_putchar('\n');
 	}
 	else
 	{
@@ -19,11 +19,11 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-					_putchar(‘\\’);
+					_putchar('\\');
 				else if (j < i)
-					_putchar(‘ ’);
+					_putchar(' ');
 			}
-			_putchar(‘\n’);
+			_putchar('\n');
 		}
 	}
 }
