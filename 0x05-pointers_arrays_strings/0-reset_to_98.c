5b7d65b8be85cb9e9aabe395bea6b1cc63548282
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-02-27 09:19:57 -0800

    function that takes a pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ *
+ * @n: integer input pointer
+ *
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
