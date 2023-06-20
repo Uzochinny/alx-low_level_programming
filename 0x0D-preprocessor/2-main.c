949721f9f3702381f7360e0096e7386997449044
Chinedu Uzochukwu <chineduuzochukwu@gmail.com>
2023-06-20 15:17:22 -0700

    program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main() {
+	printf("Compiled from file: %s\n", __FILE__);
+	return 0;
+}
